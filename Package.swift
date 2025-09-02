7d9b66197fba640a962b51f73401f8344b345dcc
Paolo Rossignoli <paolo.rossignoli@azero.it>
2025-09-02 12:36:45 +0200

    fix: Use working v4.0.0a15 release and clean package name

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "VLCKit",
+  name: "VLCKitSwift",
   platforms: [
     .iOS(.v12),       // Updated minimum versions
     .tvOS(.v12),      // tvOS support
@@ -29,8 +29,8 @@ let package = Package(
     // Contains binaries for iOS, tvOS, macOS, and visionOS
     .binaryTarget(
       name: "VLCKit",
-      url: "https://github.com/paoloandrea/vlckit-package/releases/download/4.0.1/VLCKit.xcframework.zip",
-      checksum: "ab08f7520f33b15d66a13f6c69dfc21b43f9d3733c7b5557d8dbb9b1acc5efe0" // Will be updated after multi-platform build
+      url: "https://github.com/paoloandrea/vlckit-package/releases/download/v4.0.0a15/VLCKit.xcframework.zip",
+      checksum: "ab08f7520f33b15d66a13f6c69dfc21b43f9d3733c7b5557d8dbb9b1acc5efe0"
     ),
   ]
 )
